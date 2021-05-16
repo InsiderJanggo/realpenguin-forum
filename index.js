df6b66946020514505b10484e26b4864ea7e9ac8
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2021-05-16 14:43:44 +0700

    added locales

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const lingua  = require('lingua');
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,19 @@ app.use(cookieParser());
 app.use(express.static('public'));
 //SET EJS JADI RENDER HALAMAN DEFAULT
 app.set(require("ejs").renderFile);
+
+app.use(lingua(app, {
+    defaultLocale: 'en',
+    path: __dirname + '/locales',
+    storageKey: 'lang', // http://localhost:9000/?lang=ja
+    /*cookieOptions: {
+        domain: 'localhost:9000',    // to allow subdomains access to the same cookie, for instance
+        path: '/',            // to restrict the language cookie to a path
+        httpOnly: false,          // if you need access to this cookie from javascript on the client
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),  // expire in 1 day instead of 1 year
+        secure: false              // for serving over https
+    }*/
+}));
 
 //GET REQUEST BUAT http://localhost:9000/
 app.get("/", (req, res) => {
