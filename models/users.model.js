19647d6e13375ff8355203459c9d5a37345439f1
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2021-05-19 00:00:40 +0700

    add error handler
    update locales file

--- models/users.model.js
+++ models/users.model.js
@@ -5,7 +5,17 @@ const userSchema = new Schema({
     email: String,
     password: String,
     fullname: String,
-    userID: Number
+    userID: Number,
+    createdDate: { type: Date, default: Date.now }
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.hash;
+    }
 });
 
 module.exports = model("Users", userSchema);
